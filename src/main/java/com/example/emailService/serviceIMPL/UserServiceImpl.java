41c2e832e0fc175531e3d26d5637d08dff9bbedb
samuelshola777 <samuelshola14@gmail.com>
2023-08-07 07:39:44 +0100

    practising method chain

--- src/main/java/com/example/emailService/serviceIMPL/UserServiceImpl.java
+++ src/main/java/com/example/emailService/serviceIMPL/UserServiceImpl.java
@@ -1,6 +1,9 @@
 package com.example.emailService.serviceIMPL;
 
+import com.example.emailService.Exception.UserException;
+import com.example.emailService.data.model.Confirmation;
 import com.example.emailService.data.model.User;
+import com.example.emailService.data.repository.ConfirmationRepository;
 import com.example.emailService.data.repository.UserRepository;
 import com.example.emailService.services.UserService;
 import lombok.RequiredArgsConstructor;
@@ -10,10 +13,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final ConfirmationRepository confirmationRepository;
     @Override
     public User saverUser(User user) {
-        if(userRepository.existsByEmail(user.getEmail())) throw new
-        return null;
+        if(userRepository.existsByEmail(user.getEmail())) throw new UserException(("email address already in use"));
+        user.setEnabled(false);
+        confirmationRepository.save(new Confirmation(userRepository.save(user)));        return null;
     }
 
     @Override
