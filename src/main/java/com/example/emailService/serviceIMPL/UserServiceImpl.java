287dab5acfdcaacc5fb481a84ab28d574844a158
samuelshola777 <samuelshola14@gmail.com>
2023-08-09 15:21:26 +0100

    user registration method refactoring

--- src/main/java/com/example/emailService/serviceIMPL/UserServiceImpl.java
+++ src/main/java/com/example/emailService/serviceIMPL/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.emailService.data.model.Confirmation;
 import com.example.emailService.data.model.User;
 import com.example.emailService.data.repository.ConfirmationRepository;
 import com.example.emailService.data.repository.UserRepository;
+import com.example.emailService.services.EmailService;
 import com.example.emailService.services.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,12 +17,15 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final ConfirmationRepository confirmationRepository;
+    private final EmailService emailService;
     @Override
     public User saverUser(User user) {
         if(userRepository.existsByEmail(user.getEmail())) throw new UserException(("email address already in use"));
         user.setEnabled(false);
-        user =  confirmationRepository.save(new Confirmation(userRepository.save(user))).getUser();
+     User savedUser =userRepository.save(user);
+      Confirmation  savedConfirmation =  confirmationRepository.save(new Confirmation(savedUser));
         // TODO send email notification with token
+        emailService.sendSimpleMailMessage("boneshaker",user.getEmail(),savedConfirmation.getToken());
         return user;
     }
 
